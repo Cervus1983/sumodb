fba3ccb161c7aaa4eaf863ef360df470ca7694e1
Mikhail Zhilkin <Mikhail.Zhilkin@kambi.com>
2018-01-08 17:30:51 +0100

    all tournaments since 2000

--- data.world.script.R
+++ data.world.script.R
@@ -3,9 +3,10 @@ set_config(saved_cfg)
 
 source("sumodb.R")
 
-# 6 years ~ 36 tournaments
-basho <- expand.grid(2012:2017, sprintf("%02d", seq(1, 12, by = 2))) %>% 
+# 18 years ~ 107 tournaments (2011 March cancelled)
+basho <- expand.grid(2000:2017, sprintf("%02d", seq(1, 12, by = 2))) %>% 
 	apply(., 1, paste, collapse = ".") %>% 
+	setdiff(., "2011.03") %>% 
 	sort()
 
 # https://data.world/cervus/sumo-wrestling-banzuke
