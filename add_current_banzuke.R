7f14ec7141de4927494b63491c9d1f6d4f4cba2a
Cervus1983 <cervus@ip-172-31-16-12.eu-central-1.compute.internal>
2018-01-15 22:51:36 +0000

    .

---
+++ add_current_banzuke.R
@@ -0,0 +1,20 @@
+if (lubridate::month(Sys.Date()) %% 2 == 1) {
+	library(data.world)
+	set_config(cfg_env(auth_token_var = "DW_API_TOKEN"))
+	
+	uploaded <- map_chr(
+		get_dataset("cervus/sumo-wrestling-banzuke")$files,
+		"name"
+	) %>% 
+		gsub("\\.csv$", "", .)
+	
+	source("sumodb.R")
+	
+	basho <- format(Sys.Date(), "%Y.%m")
+
+	if (!(basho %in% uploaded)) upload_data_frame(
+		dataset = "cervus/sumo-wrestling-banzuke",
+		data_frame = sumodbBanzuke(basho),
+		file_name = paste(basho, "csv", sep = ".")
+	)
+}
